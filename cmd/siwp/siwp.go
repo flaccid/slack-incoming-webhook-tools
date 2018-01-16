4ccc148f251bd6c00d7e51a404322d3303c5a75c
Chris Fordham <cfordham@scentregroup.com>
2018-01-16 20:42:50 +1100

    Parameterise listen port and add in env var support.

--- cmd/siwp/siwp.go
+++ cmd/siwp/siwp.go
@@ -28,12 +28,19 @@ func main() {
 	app.Before = beforeApp
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "webhook-url,t",
-			Usage: "url of the slack webhook",
+			Name:   "webhook-url,t",
+			Usage:  "url of the slack webhook",
+			EnvVar: "SLACK_WEBHOOK_URL",
+		},
+		cli.StringFlag{
+			Name:   "listen-port,p",
+			Usage:  "port to listen for requests on",
+			Value:  "8080",
+			EnvVar: "LISTEN_PORT",
 		},
 		cli.BoolFlag{
-			Name:  "debug,d",
-			Usage: "run in debug mode",
+			Name:   "debug,d",
+			Usage:  "run in debug mode",
 		},
 	}
 	app.Run(os.Args)
@@ -47,7 +54,7 @@ func start(c *cli.Context) error {
 		os.Exit(1)
 	}
 
-	proxy.Serve(c.String("webhook-url"), "8000")
+	proxy.Serve(c.String("webhook-url"), c.String("listen-port"))
 
 	return nil
 }
